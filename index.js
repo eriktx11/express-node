46e6b5d9b032bf22182e7ede538c5099971dc996
eriktx11 <erik@llerenapiano.com>
2019-05-28 23:05:45 -0500

    Memorizing code

--- index.js
+++ index.js
@@ -4,32 +4,38 @@ const hostname = 'localhost';
 const fs = require('fs');
 const path = require('path');
 
+const server = http.createServer((req, res) => {
 
-const server = http.createServer((res,req) => {
-    
-    if(req.method == 'GET'){
+    if(req.method=='GET'){
         var fileUrl;
-        if(req.url =='/') fileUrl = './index.html';
+        if (req.url=='/') fileUrl ='/index.html' ;
         else fileUrl = req.url;
 
-        var filePath = path.resolve('./public'+fileUrl);
-        const fileExt = path.extname(filePath);
-        
-        if(fileExt == '.html'){
-            fs.exists(filePath, (exists)=>{
+            var filePath = path.resolve('./public'+fileUrl);
+            const fileExt = path.extname(filePath);
+            if( fileExt == '.html')
+            {
+            fs.exists(filePath, (exists)=> {
                 if(!exists){
-                    res.statusCode = 404;
-                    console.log("Valid ");
-                    res.setHeader('Content-type', 'text/html');
-                    res.end('<html><body></h1>Nothing here</h1></doby></html>');
+                    res.statusCode=404;
+                    res.setHeader('Constant-type', 'html/text');
+                    res.end('<html>errror</html>');
+    
                     return;
-                    }
-            });
-        }
+                }
+                res.statusCode=200;
+                res.setHeader('Constant-type', 'html/text');
+                fs.createReadStream(filePath).pipe(res);
+            })
+            return;
 
+        }
+        
     }
 
-})
 
-server.listen(port,hostname);
-console.log("server running");
+});
+
+server.listen(port, hostname, () =>{
+    console.log("running...")
+})
