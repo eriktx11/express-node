b95c3fa8b1e94f369b4ec4e31d9009040ab73a4e
eriktx11 <erik@llerenapiano.com>
2019-05-29 21:08:33 -0500

    [Mod]: Switching to Express

--- index.js
+++ index.js
@@ -1,35 +1,17 @@
-const http = require('http');
+const express = require('express');
 const port = 3000;
 const hostname = 'localhost';
-const fs = require ('fs');
-const path = require ('path');
+const http = require('http');
 
-const server = http.createServer((req, res) => {
-    if(req.method == 'GET'){
-        var fileName;
-        if(req.url == '/') fileName = '/index.html';
-        else fileName = req.url;
+const app = express();
 
-        var filePath  = path.resolve('./public'+fileName);
-        const fileExt = path.extname(filePath);
-        if(fileExt == '.html'){
-            fs.exists(filePath, (exists)=>{
-                if(!exists){
-                    res.statusCode = 404;
-                    res.setHeader('Content-type','text/html');
-                    res.end('<html>doesnt exist</html>');
-
-                    return;
-                }
-                res.statusCode = 200;
-                res.setHeader('Content-type','text/html');
-                fs.createReadStream(filePath).pipe(res);
-            });
-           
-        }
-        
-    }
+app.use((req, res, next)=>{
+    res.statusCode = 200;
+    res.setHeader('Content-type','text/html');
+    res.end('<html>Express server</html>');
 });
+
+const server = http.createServer(app);
 
 server.listen(port, hostname, ()=>{
     console.log('Listen');
