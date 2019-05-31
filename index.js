22d5d575821af2d73fe0b0e5100c1be5cfcce48a
eriktx11 <erik@llerenapiano.com>
2019-05-30 22:33:35 -0500

    [Feat] Setting server with Morgan

--- index.js
+++ index.js
@@ -3,18 +3,40 @@ const http = require('http');
 const port = 3000;
 const hostname = 'localhost';
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const app = express();
-app.use(morgan('dev'));
+app.use(morgan ('dev'));
+app.use(bodyParser.json());
+
+app.all('/dishes',(req, res, next) => {
+    res.statusCode =200;
+    res.setHeader('Content-type', 'text/plain');
+    next();
+});
+
+app.get('/dishes', (req, res, next)=>{
+    res.end('Will do that');
+
+});
+
+app.post('/dishes', (req, res, next)=> {
+    res.end('Will add'+req.body.esteVal);
+});
+
+app.get('/dishes/:stockId', (req, res, next)=>{
+    res.end('Will do that with id with end');
+});
+
+
+
 app.use(express.static(__dirname+'/public'));
 
-app.use((req, res, next) =>{
+app.use((req, res, next)=>{
     res.statusCode=200;
     res.setHeader('Content-type','text/html');
     res.end('<html>express</html>');
 });
-
-
 
 const server = http.createServer(app);
 
